c46d4da41b2b66f37d0a6fa9f256383f7a75e423
Forrest Gasdia <EP-Guy@users.noreply.github.com>
2019-10-29 23:30:17 -0600

    Docstring formatting, switch to test-specific dependencies

--- src/ModifiedHankelFunctionsOfOrderOneThird.jl
+++ src/ModifiedHankelFunctionsOfOrderOneThird.jl
@@ -9,33 +9,35 @@ using Markdown
 export modifiedhankel
 
 # Number of terms to sum in both `powerseries` and `asymptotic` solutions
-# Larger `numterms` generate successively smaller values, but can lead to floating point
+# Larger `NUMTERMS` generate successively smaller values, but can lead to floating point
 # errors when multiplied against successively larger powers of `z`.
-const numterms = 30
+const NUMTERMS = 30
 
 # Series for auxiliary functions `f`, `f′`, `g`, and `g′` in `powerseries`.
 const a₀ = cbrt(2)/gamma(2/3)
 a(m) = m == 0 ? a₀ : -a(m-1)/((3m-1)*3m)
-const avec = @SVector [a(i) for i = 0:numterms]
+const avec = @SVector [a(i) for i = 0:NUMTERMS]
 
 const b₀ = cbrt(2)/(3^(2/3)*gamma(4/3))
 b(m) = m == 0 ? b₀ : -b(m-1)/((3m+1)*3m)
-const bvec = @SVector [b(i) for i = 0:numterms]
+const bvec = @SVector [b(i) for i = 0:NUMTERMS]
 
 const c₀ = a₀/2
 c(m) = m == 0 ? c₀ : -c(m-1)/((3m+2)*3m)
-const cvec = @SVector [c(i) for i = 0:numterms]
+const cvec = @SVector [c(i) for i = 0:NUMTERMS]
 
 const d₀ = b₀
 d(m) = m == 0 ? d₀ : -d(m-1)/((3m-2)*3m)
-const dvec = @SVector [d(i) for i = 0:numterms]
+const dvec = @SVector [d(i) for i = 0:NUMTERMS]
 
 Cterm(m) = 9(2m - 1)^2 - 4
 C(m)::Float64 = prod(Cterm, 1:m)/(2^(4m)*3^m*factorial(m))
-const Cvec = @SVector [C(BigInt(i)) for i = 1:numterms]
+const Cvec = @SVector [C(BigInt(i)) for i = 1:NUMTERMS]
 
 
 """
+    modifiedhankel(z)
+
 Return h₁, h₂, h₁′, and h₂′, the first and second modified Hankel functions of order 1/3 and
 their derivatives.
 
@@ -43,9 +45,14 @@ These functions solve Stokes' equation ``d²u/dz² + zu = 0`` as a power series 
 `abs(z) < 6` and an approximate asymptotic expansion otherwise. The asymptotic solution is
 necessary because the ``z³ⁱ`` in the power series blows up as ``i → ∞``.
 
-Based on Tables of the Modified Hankel Functions...
+# References
 
-See also: [powerseries](@ref), [asymptotic](@ref)
+[^1]:
+
+    *Tables of the modified Hankel function of order one-third and of their derivatives*,
+    The Staff of the Computation Library, Harvard University, 1945.
+
+See also: [`powerseries`](@ref), [`asymptotic`](@ref)
 """
 function modifiedhankel(z)
     if abs2(z) < 36
@@ -66,9 +73,16 @@ Return h₁, h₂, h₁′, and h₂′, the first and second modified Hankel fu
 their derivatives using a *power series*.
 
 These functions solve Stokes' equation ``d²u/dz² + zu = 0`` as a power series in ``z``,
-valid in the entire complex plane. Based on Tables of the Modified Hankel Functions...
+valid in the entire complex plane.
 
-See also: [modifiedhankel](@ref), [asymptotic](@ref)
+# References
+
+[^1]:
+
+    *Tables of the modified Hankel function of order one-third and of their derivatives*,
+    The Staff of the Computation Library, Harvard University, 1945.
+
+See also: [`modifiedhankel`](@ref), [`asymptotic`](@ref)
 """
 function powerseries(z)
     # Auxiliary functions
@@ -80,7 +94,7 @@ function powerseries(z)
 
     zterm = z^3
     zpow = one(z)
-    @inbounds for i = 2:numterms
+    @inbounds for i = 2:NUMTERMS
         zpow *= zterm  # for z^(3i)
         fval += avec[i]*zpow
         gval += bvec[i]*zpow
@@ -103,7 +117,7 @@ function powerseries(z)
 end
 
 
-doc"""
+"""
     asymptotic(z)
 
 Return h₁, h₂, h₁′, and h₂′, the first and second modified Hankel functions of order 1/3 and
@@ -113,45 +127,54 @@ Two separate functions, valid across separate ranges of ``arg(z)`` are necessary
 the full region of the complex plane. This is an example of Stokes' phenomenon, the existence
 of two expressions of different forms which represent asymptotically the same integral function.
 
-Based on Modified Hankel Functions ...
-
-For ``h₁`` on ``-2π/3 < \arg z < 4π/3``:
+For ``h₁`` on ``-2π/3 < \\arg z < 4π/3``:
 ```math
-h₁(z) ≈ α z^{-1/4} e^{2/3 i z^{3/2} - 5πi/12} \left( 1 + \sum_{m=1} (-i)^m C_m z^{-3m/2} \right)
-h₁′(z) ≈ α i z^{1/4} e^{2/3 i z^{3/2} - 5πi/12} \left( 1 + \sum_{m=1} (-i)^m C_m z^{-3m/2} \right) -
-    α/4 z^{-5/4} e^{2/3 i z^{3/2} - 5πi/12} \left( 1 + \sum_{m=1} (-i)^m C_m z^{-3m/2} \right) -
-    3/2 α z^{-1/4} e^{2/3 i z^{3/2} - 5πi/12} \left( \sum_{m=1} (-i)^m m C_m z^{-3m/2 - 1} \right)
+h₁(z) ≈ α z^{-1/4} e^{2/3 i z^{3/2} - 5πi/12} \\left( 1 + \\sum_{m=1} (-i)^m C_m z^{-3m/2} \\right)
+
+h₁′(z) ≈ α i z^{1/4} e^{2/3 i z^{3/2} - 5πi/12} \\left( 1 + \\sum_{m=1} (-i)^m C_m z^{-3m/2} \\right) -
+    α/4 z^{-5/4} e^{2/3 i z^{3/2} - 5πi/12} \\left( 1 + \\sum_{m=1} (-i)^m C_m z^{-3m/2} \\right) -
+    3/2 α z^{-1/4} e^{2/3 i z^{3/2} - 5πi/12} \\left( \\sum_{m=1} (-i)^m m C_m z^{-3m/2 - 1} \\right)
 ```
 where
 ```math
-C_m = \frac{(9-4)(81-4)\cdots (9[2m-1]^2-4)}{2^{4m}3^m m!}
+C_m = \\frac{(9-4)(81-4)\\cdots (9[2m-1]^2-4)}{2^{4m}3^m m!}
 ```
 and
 ```math
-h₁(z) ≈ h₁(z) [above] + α z^{-1/4} e^{-2/3 i z^{3/2} - 11πi/12} \left( 1 + \sum_{m_1} (i)^m C_m z^{-3m/2} \right)
-h₁′(z) ≈ h₁′(z) [above] - α z^{1/4} e^{-2/3 i z^{3/2} - 11πi/12} \left( 1 + \sum_{m_1} (i)^m C_m z^{-3m/2} \right) -
-    α/4 z^{-5/4} e^{-2/3 i z^{3/2} - 11πi/12} \left( 1 + \sum_{m_1} (i)^m C_m z^{-3m/2} \right) -
-    3/2 α z^{-1/4} e^{-2/3 i z^{3/2} - 11πi/12} \left( \sum_{m=1} i^m m C_m z^{-3m/2 - 1} \right)
-```
-for ``-4π/3 < \arg z < 0``.
+h₁(z) ≈ h₁(z) + α z^{-1/4} e^{-2/3 i z^{3/2} - 11πi/12} \\left( 1 + \\sum_{m_1} (i)^m C_m z^{-3m/2} \\right)
 
-And for ``h₂`` on ``-4π/3 < \arg z < 2π/3``:
+h₁′(z) ≈ h₁′(z) - α z^{1/4} e^{-2/3 i z^{3/2} - 11πi/12} \\left( 1 + \\sum_{m_1} (i)^m C_m z^{-3m/2} \\right) -
+    α/4 z^{-5/4} e^{-2/3 i z^{3/2} - 11πi/12} \\left( 1 + \\sum_{m_1} (i)^m C_m z^{-3m/2} \\right) -
+    3/2 α z^{-1/4} e^{-2/3 i z^{3/2} - 11πi/12} \\left( \\sum_{m=1} i^m m C_m z^{-3m/2 - 1} \\right)
+```
+for ``-4π/3 < \\arg z < 0``.
+
+And for ``h₂`` on ``-4π/3 < \\arg z < 2π/3``:
 ```math
-h₂(z) ≈ α z^{-1/4} e^{-2/3 i z^{3/2} + 5πi/12} \left( 1 + \sum_{m=1} (i)^m C_m z^{-3m/2} \right)
-h₂′(z) ≈ -α z^{1/4} e^{-2/3 i z^{3/2} + 5πi/12} \left( 1 + \sum_{m=1} (i)^m C_m z^{-3m/2} \right) -
-    α/4 z^{-5/4} e^{-2/3 i z^{3/2} + 5πi/12} \left( 1 + \sum_{m=1} (i)^m C_m z^{-3m/2} \right) -
-    3/2 α z^{-1/4} e^{-2/3 i z^{3/2} + 5πi/12} \left( i^m m C_m z^{-3m/2 - 1} \right)
+h₂(z) ≈ α z^{-1/4} e^{-2/3 i z^{3/2} + 5πi/12} \\left( 1 + \\sum_{m=1} (i)^m C_m z^{-3m/2} \\right)
+
+h₂′(z) ≈ -α z^{1/4} e^{-2/3 i z^{3/2} + 5πi/12} \\left( 1 + \\sum_{m=1} (i)^m C_m z^{-3m/2} \\right) -
+    α/4 z^{-5/4} e^{-2/3 i z^{3/2} + 5πi/12} \\left( 1 + \\sum_{m=1} (i)^m C_m z^{-3m/2} \\right) -
+    3/2 α z^{-1/4} e^{-2/3 i z^{3/2} + 5πi/12} \\left( i^m m C_m z^{-3m/2 - 1} \\right)
 ```
 and
 ```math
-h₂(z) ≈ h₂(z) [above] + α z^{-1/4} e^{2/3 i z^{3/2} + 11πi/12} \left( 1 + \sum_{m=1} (-i)^m C_m z^{-3m/2} \right)
-h₂′(z) ≈ h₂′(z) [above] + α z^{1/4} e^{2/3 i z^{3/2} + 11πi/12} \left( 1 + \sum_{m=1} (-i)^m C_m z^{-3m/2} \right) -
-    α/4 z^{-5/4} e^{2/3 i z^{3/2} + 11πi/12} \left( 1 + \sum_{m=1} (-i)^m C_m z^{-3m/2} \right) -
-    3/2 α z^{-1/4} e^{2/3 i z^{3/2} + 11πi/12} \left( \sum_{m=1} (-i)^m m C_m z^{-3m/2 - 1} \right)
-```
-for ``0 < \arg z < 4π/3``.
+h₂(z) ≈ h₂(z) + α z^{-1/4} e^{2/3 i z^{3/2} + 11πi/12} \\left( 1 + \\sum_{m=1} (-i)^m C_m z^{-3m/2} \\right)
 
-See also: [modifiedhankel](@ref), [powerseries](@ref)
+h₂′(z) ≈ h₂′(z) + α z^{1/4} e^{2/3 i z^{3/2} + 11πi/12} \\left( 1 + \\sum_{m=1} (-i)^m C_m z^{-3m/2} \\right) -
+    α/4 z^{-5/4} e^{2/3 i z^{3/2} + 11πi/12} \\left( 1 + \\sum_{m=1} (-i)^m C_m z^{-3m/2} \\right) -
+    3/2 α z^{-1/4} e^{2/3 i z^{3/2} + 11πi/12} \\left( \\sum_{m=1} (-i)^m m C_m z^{-3m/2 - 1} \\right)
+```
+for ``0 < \\arg z < 4π/3``.
+
+# References
+
+[^1]:
+
+    *Tables of the modified Hankel function of order one-third and of their derivatives*,
+    The Staff of the Computation Library, Harvard University, 1945.
+
+See also: [`modifiedhankel`](@ref), [`powerseries`](@ref)
 """
 function asymptotic(z)
     α = cbrt(2)*3^(1/6)/sqrt(π)
@@ -167,7 +190,7 @@ function asymptotic(z)
     t = one(z)
     sp = zero(z)
     tp = zero(z)
-    @inbounds for i = 1:numterms
+    @inbounds for i = 1:NUMTERMS
         zpower *= zterm
         negative_zpower *= negative_zterm
 
