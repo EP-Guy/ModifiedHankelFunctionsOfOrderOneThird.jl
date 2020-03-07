9a9537a24ff25a4065a4572d13c9e7821c2b1077
Forrest Gasdia <fgasdia@users.noreply.github.com>
2020-03-07 10:15:58 -0700

    Add test comparison to Airy and Bessel functions

--- test/ModifiedHankelFunctionTests.jl
+++ test/ModifiedHankelFunctionTests.jl
@@ -1,5 +1,6 @@
 using Test
 using ModifiedHankelFunctionsOfOrderOneThird
+using SpecialFunctions
 
 @testset "Zeros of h₂" begin
     h1, h2, h1p, h2p = modifiedhankel(complex(-1.16905371, 2.02486041))
@@ -55,6 +56,72 @@ end
     @test h2p ≈ complex(-176.03846956, 196.31112124)
 end
 
+@testset "Comparison with Airy and Bessel functions" begin
+    z = complex(1.345,-0.245)
+    T = 2/3*z^(3/2)
+    k = (3/2)^(2/3)*(1 - im*sqrt(3)/3)
+    β = 2^(4/3)*3^(1/6)
+
+    h1, h2, h1p, h2p = modifiedhankel(z)
+    h1m, h2m, h1mp, h2mp = modifiedhankel(-z)
+    h1c, h2c, h1cp, h2cp = modifiedhankel(conj(z))
+
+    ai, bi = airyai(z), airybi(z)
+    aim, bim = airyai(-z), airybi(-z)
+    aip, bip = airyaiprime(z), airybiprime(z)
+
+    # See SCL1945 (22), (25)
+    @test h2 ≈ conj(h1c)
+    @test h2p ≈ conj(h1cp)
+
+    # SCL1945 (18), (19)
+    @test h1 ≈ k*(aim - im*bim)
+    @test h2 ≈ conj(k)*(aim + im*bim)
+
+    # SCL1945 (20), (21)
+    @test ai ≈ 1/2k*h1m + 1/(2*conj(k))*h2m
+    @test bi ≈ im/2k*h1m - im/(2*conj(k))*h2m
+
+    # SCL1945 (23), (24)
+    @test aip ≈ -1/(2k)*h1mp - 1/(2*conj(k))*h2mp
+    @test bip ≈ -im/(2k)*h1mp + im/(2*conj(k))*h2mp
+
+    # SCL1945 (26)
+    h1t = modifiedhankel(cis(-2π/3)*z)[1]
+    h2t = modifiedhankel(cis(2π/3)*z)[2]
+    @test -im*β*aim ≈ h1t
+    @test -im*β*aim ≈ -h2t
+    @test -im*β*aim ≈ -cis(π/3)*(cis(π/3)*h1 + h2)
+
+    # SCL1945 (27)
+    h1t = modifiedhankel(conj(z)*exp(2π*im/3))[1]
+    @test h1t ≈ -h2c
+    @test h1t ≈ -conj(h1)
+
+    # SCL1945 (28)
+    h1tp = modifiedhankel(z*exp(2π*im/3))[3]
+    @test h1tp ≈ cis(π/3)*h2p
+    @test h1tp ≈ cis(π/3)*conj(h1cp)
+
+    # SCL1945
+    @test besselj(1/3,T) ≈ 1/2*T^(-1/3)*(h1 + h2)  # (36)
+    @test bessely(1/3,T) ≈ 1/2im*T^(-1/3)*(h1 - h2)  # (37)
+    @test hankelh1(1/3,T) ≈ T^(-1/3)*h1  # (38)
+    @test hankelh2(1/3,T) ≈ T^(-1/3)*h2  # (39)
+    @test besselj(-1/3,T) ≈ 1/2*T^(-1/3)*(cis(π/3)*h1 + cis(-π/3)*h2)  # (40)
+    @test bessely(-1/3,T) ≈ 1/2im*T^(-1/3)*(cis(π/3)*h1 - cis(-π/3)*h2)  # (41)
+    @test hankelh1(-1/3,T) ≈ cis(π/3)*T^(-1/3)*h1  # (42)
+    @test hankelh2(-1/3,T) ≈ cis(-π/3)*T^(-1/3)*h2  # (43)
+    @test besselj(2/3,T) ≈ 1/2*(2/3)^(1/3)*T^(-2/3)*(cis(-2π/3)*h1p + cis(2π/3)*h2p)  # (44)
+    @test bessely(2/3,T) ≈ 1/2im*(2/3)^(1/3)*T^(-2/3)*(cis(-2π/3)*h1p - cis(2π/3)*h2p)  # (45)
+    @test hankelh1(2/3,T) ≈ (2/3)^(1/3)*cis(-2π/3)*T^(-2/3)*h1p  # (46)
+    @test hankelh2(2/3,T) ≈ (2/3)^(1/3)*cis(2π/3)*T^(-2/3)*h2p  # (47)
+    @test besselj(-2/3,T) ≈ 1/2*(2/3)^(1/3)*T^(-2/3)*(h1p + h2p)  # (48)
+    @test bessely(-2/3,T) ≈ 1/2im*(2/3)^(1/3)*T^(-2/3)*(h1p - h2p)  # (49)
+    @test hankelh1(-2/3,T) ≈ (2/3)^(1/3)*T^(-2/3)*h1p  # (50)
+    @test hankelh2(-2/3,T) ≈ (2/3)^(1/3)*T^(-2/3)*h2p  # (51)
+end
+
 @testset "Asymptotic expansion" begin
     @test ModifiedHankelFunctionsOfOrderOneThird.C(1) ≈ 0.10416666666666667
     @test ModifiedHankelFunctionsOfOrderOneThird.C(2) ≈ 0.08355034722222222
@@ -62,7 +129,7 @@ end
     @test ModifiedHankelFunctionsOfOrderOneThird.C(7) ≈ 14.9957629868626
     @test ModifiedHankelFunctionsOfOrderOneThird.C(BigInt(8)) ≈ 78.923013011587
     @test ModifiedHankelFunctionsOfOrderOneThird.C(BigInt(11)) ≈ 24086.5496
-    @test ModifiedHankelFunctionsOfOrderOneThird.C(BigInt(14)) ≈ 17484377.
+    @test ModifiedHankelFunctionsOfOrderOneThird.C(BigInt(14)) ≈ 17484377.0
     @test ModifiedHankelFunctionsOfOrderOneThird.C(BigInt(25)) ≈ 7.3900049415704853993e+19
 end
 
@@ -73,9 +140,9 @@ end
     # mh1lwpc = modhankelexpansion(z, false)
     h1, h2, h1p, h2p = ModifiedHankelFunctionsOfOrderOneThird.asymptotic(z)
 
-    @test h1 ≈ complex(0.3822204,-0.0108716) *exp(-expz) rtol=1e-5
-    @test h2 ≈ complex(0.3823441,-0.0102396) *exp(expz) rtol=1e-5
-    @test h1p ≈ complex(-0.0548274,1.9051478) *exp(-expz) rtol=1e-5
+    @test h1 ≈ complex(0.3822204,-0.0108716)*exp(-expz) rtol=1e-5
+    @test h2 ≈ complex(0.3823441,-0.0102396)*exp(expz) rtol=1e-5
+    @test h1p ≈ complex(-0.0548274,1.9051478)*exp(-expz) rtol=1e-5
     @test h2p ≈ complex(0.0503774,-1.9045298)*exp(expz) rtol=1e-5
 
     z = complex(21.7820282,2.7361517)
